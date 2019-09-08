97614033f0cb2844e2c268864f6a7e16abc95d75
lichenchao1996 <lichenchao1996@gmail.com>
2019-09-08 10:24:54 +0800

    spring boot 配置文件位置及加载顺序
    Signed-off-by: lichenchao1996 <lichenchao1996@gmail.com>

---
+++ spring-boot-config-load/src/main/java/com/licc/springbootconfigload/SpringBootConfigLoadApplication.java
@@ -0,0 +1,13 @@
+package com.licc.springbootconfigload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigLoadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfigLoadApplication.class, args);
+    }
+
+}
