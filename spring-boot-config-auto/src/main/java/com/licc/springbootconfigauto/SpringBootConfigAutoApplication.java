36bda18943be175cf3a86769bf95032ad6befc72
lichenchao1996 <lichenchao1996@gmail.com>
2019-09-13 23:33:34 +0800

    spring boot 自动配置解析
    Signed-off-by: lichenchao1996 <lichenchao1996@gmail.com>

---
+++ spring-boot-config-auto/src/main/java/com/licc/springbootconfigauto/SpringBootConfigAutoApplication.java
@@ -0,0 +1,13 @@
+package com.licc.springbootconfigauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfigAutoApplication.class, args);
+    }
+
+}
